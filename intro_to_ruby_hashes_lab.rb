feb666dd6947b1da36a016f9dedd30657f81bb93
Tish Chungoora <tishal82@gmail.com>
2019-09-17 13:09:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
-	    :railroads => {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
+	    :railroads => {reading_railroad: {mortgage_value: }, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
 	  }
 	}
 end
