6008de595e4ea4b727c369ddf1fea0ad6b28b5d2
Tish Chungoora <tishal82@gmail.com>
2019-09-17 13:16:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_fourth_tier
 	      three_pieces_owned: 100, 
 	      four_pieces_owned: 200
 	    }
-	    :railroads => {
+	    :names => {
 	      reading_railroad: {mortgage_value: "$100"}, 
 	      pennsylvania_railroad: {mortgage_value: "$200"}, 
 	      b_and_o_railroad: {mortgage_value: "$400"}, 
